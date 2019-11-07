8e8fa0cdca57976dd626140b9496bbc84fe78004
f544 <pilaolympus@gmail.com>
2019-11-07 10:25:01 +0700

    file.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+# print ('hai')
+## untuk commit pake git rm --cached 1.py
+##git commit -m "file.py"
+class X :
+    def __init__ (self) :
+        self.nama = 'Andi'
+
+Andi = X()
+print (Andi.nama)
+
+a = 12
+print (a + a)
